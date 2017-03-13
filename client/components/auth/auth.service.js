07322272302222b69bb252a27cbf5bedff087534
Mohamed hedi Nasr <mohamedhedi.nasr@esprit.tn>
2017-03-13 17:25:41 +0100

    User can't login
    Problem solved

--- client/components/auth/auth.service.js
+++ client/components/auth/auth.service.js
@@ -17,29 +17,27 @@ angular.module('shopnxApp')
        * @param  {Function} callback - optional
        * @return {Promise}
        */
-      login: function(user, callback) {
+     login: function(user, callback) {
         var cb = callback || angular.noop;
         var deferred = $q.defer();
 
         $http.post('/auth/local', {
           email: user.email,
           password: user.password
-        }).
-        success(function(data) {
-          $cookieStore.put('token', data.token);
+        }).then(success, error);
+        function success(data) {
+          $cookieStore.put('token', data.data.token);
           currentUser = User.get();
           deferred.resolve(data);
           return cb();
-        }).
-        error(function(err) {
+        }
+        function error(err) {
           this.logout();
           deferred.reject(err);
           return cb(err);
-        }.bind(this));
-
+        }
         return deferred.promise;
       },
-
       /**
        * Delete access token and user info
        *
