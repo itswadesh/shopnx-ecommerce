ffe5ae0e30e19df059a207658702701304efbd6f
Mohamed hedi Nasr <mohamedhedi.nasr@esprit.tn>
2017-04-28 14:11:46 +0100

    Update user

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -12,6 +12,7 @@ router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
+router.get('/:id', auth.isAuthenticated(), controller.update);
 router.post('/', controller.create);
 
 module.exports = router;
